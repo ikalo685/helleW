a5ba7d98ed5812d5f28dc6b9951371b29ba9d61f
Prince <alvinprinceikalo01021998@gmail.com>
2023-03-10 22:06:51 +0300

    montant

--- apply.py
+++ apply.py
@@ -1,6 +1,9 @@
 #formulaire et ate de naissance
-name = input("Quel est votre nom ?")
-date = input("Quelle est votre annee de naissance ?")
+name = input("Quel est votre nom ? ")
+date = input("Quelle est votre annee de naissance ? ")
 age = 2023 - int(date)
 sport_favorit = input("Quel est votre sport favorit? ")
-print("Bonjour " + name, "vous avez", age, "ans et vous aimer le ")
+montant_rouble = input("montant en rouble ")
+dollar = 1*int(montant_rouble)/73.41
+print("Bonjour " + name, "vous avez", age, "ans, vous aimer le", sport_favorit,"et vous avez", (montant_rouble), "roubles","ce qui vaut", dollar,"$")
+print("maintenat nous faisons les choses bien a", int(age), "ans")
