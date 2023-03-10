a0d6b7dc3ea31e99d9a6d0188a3c5619a567115b
Prince <alvinprinceikalo01021998@gmail.com>
2023-03-10 19:55:50 +0300

    formulaire

---
+++ apply.py
@@ -0,0 +1,6 @@
+#formulaire et ate de naissance
+name = input("Quel est votre nom ?")
+date = input("Quelle est votre annee de naissance ?")
+age = 2023 - int(date)
+sport_favorit = input("Quel est votre sport favorit? ")
+print("Bonjour " + name, "vous avez", age, "ans et vous aimer le ")
